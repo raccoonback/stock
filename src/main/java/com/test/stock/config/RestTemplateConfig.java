0b386060adc74227e91e94f96d045711785a0473
Seung Bin . Ko <kosb15@naver.com>
2020-10-21 07:26:59 +0900

    feat: RestTemplate의 HTTP 연결 시간, 읽기 시간 제한 설정 추가
    - 연결 시간: 2초 이내
    - 읽기 시간: 15초 이내

--- src/main/java/com/test/stock/config/RestTemplateConfig.java
+++ src/main/java/com/test/stock/config/RestTemplateConfig.java
@@ -1,5 +1,7 @@
 package com.test.stock.config;
 
+import java.time.Duration;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,10 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 	@Bean
 	public RestTemplate setUpRestTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+		return builder
+			.setReadTimeout(Duration.ofSeconds(15))
+			.setConnectTimeout(Duration.ofSeconds(2))
+			.setBufferRequestBody(true)
+			.build();
 	}
 }
