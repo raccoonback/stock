75bb0dce4242aca6d39cdd679082565fac30467a
Seung Bin . Ko <kosb15@naver.com>
2020-10-21 00:29:57 +0900

    fix: feedback 함수에서 api, symbol 참조할 수 있도록 파리미터 추가

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -9,11 +9,11 @@ function validate(symbol) {
     return !symbol && !symbol.length;
 }
 
-function feedback(response) {
+function feedback(api, symbol, response) {
     if (response.status === 400) {
-        throw Error(`${symbol}은 존재하지 않는 종목입니다. 다시 확인해주세요.`);
+        throw new Error(`${symbol}은 존재하지 않는 종목입니다. 다시 확인해주세요.`);
     } else if (response.status === 500) {
-        throw Error(`${api}에서 데이터를 조회하는 과정에서 예기치 않은 에러가 발생했습니다. 다시 시도해주세요.`);
+        throw new Error(`${api}에서 데이터를 조회하는 과정에서 예기치 않은 에러가 발생했습니다. 다시 시도해주세요.`);
     }
 }
 
@@ -23,7 +23,7 @@ async function getTimeSerialAndProfit(api, symbol) {
     }
 
     const response = await requestApi(`/api/stock/${api}/${symbol}`);
-    feedback(response);
+    feedback(api, symbol, response);
     const {stockProfit, stockPrices} = await response.json();
     return {
         dates: stockPrices.map(it => it.date),
